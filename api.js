3e141fc094148cfdb0f55c1062821f4fc1dd6756
DevNinja <kenjiakira2006@gmail.com>
2024-12-27 15:00:14 +0700

    Vercel API testing

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
+const { Server } = require('socket.io');
 const cors = require('cors');
 const path = require('path');
 const app = express();
@@ -9,48 +9,45 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 const imgurImages = [
 "https://i.imgur.com/FNRRTy7.jpg",];
 
 let sentImages = [];
 
-const server = http.createServer(app); 
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws) => {
-    console.log('New WebSocket connection established.');
+io.on('connection', (socket) => {
+    console.log('New Socket.IO connection established.');
     
-    ws.send('WebSocket connection established. Ready to receive logs or image requests.');
+    socket.emit('message', 'Socket.IO connection established. Ready to receive logs or image requests.');
     
-    ws.on('message', (message) => {
-        console.log(`Received message: ${message}`);
-
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
     });
 });
 
 app.get('/getRandomImage', (req, res) => {
- 
     const availableImages = imgurImages.filter(img => !sentImages.includes(img));
 
     if (availableImages.length === 0) {
-     
         sentImages = [];
         return res.json({ error: 'Hết ảnh để gửi, danh sách ảnh đã được làm mới.' });
     }
 
     const randomIndex = Math.floor(Math.random() * availableImages.length);
     const randomImage = availableImages[randomIndex];
-
     sentImages.push(randomImage);
 
     res.json({ imageUrl: randomImage });
 
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(`New image sent: ${randomImage}`);
-        }
-    });
+    io.emit('newImage', `New image sent: ${randomImage}`);
 });
 
 app.get('*', (req, res) => {
